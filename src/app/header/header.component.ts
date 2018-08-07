14f13a35e9a4eaf30321099fde9ea75d87b4fa60
borzunoved <borzunoved@altarix.ru>
2018-08-07 12:20:33 +0300

    добавлены декораторы, рабочий эвент эмиттер

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class HeaderComponent {
+  public test;
+  public menuItems: { id: number, title: string }[] = [{id: 1, title: 'Первый раздел'}, {id: 2, title: 'Второй раздел'}];
+  public menuItem: any;
+  myFunc = item => {
+    console.log(item);
   }
-
 }
--- src/app/header/nav/nav.component.ts
+++ src/app/header/nav/nav.component.ts
@@ -1,16 +1,20 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-
-export class NavComponent {
-  constructor () {}
-  @Output() changed = new EventEmitter<string>();
-  change(currentLink: any) {
-    this.changed.emit(currentLink);
+export class NavComponent implements OnInit {
+  @Input() menuItem: { id: number, title: string }[];
+  @Output() changed = new EventEmitter<{id: number, title: string}>();
+  @Output() anotherChanged = new EventEmitter<any>();
+  setItem(e: Event, item: {id: number, title: string}): void {
+    e.preventDefault();
+    this.changed.emit(item);
+  }
+  ngOnInit() {
+    this.anotherChanged.emit(123);
   }
 }
 
